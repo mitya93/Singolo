39ac44cda6b2d914252d75cee578c3984d5c2bc5
mitya93 <dimakovalvdv@mail.ru>
2020-03-21 23:56:44 +0300

    feat: add scripts

--- script.js
+++ script.js
@@ -50,3 +50,46 @@ document.querySelector('.right.chev_r').addEventListener('click', function () {
         nextSlide(currentSlider);
     }
 });
+
+
+
+
+const PORTFOLIO = document.getElementById('portfolio-images');
+const PORTFOLIO_TAGS = document.getElementById('portfolio-tags');
+
+PORTFOLIO_TAGS.addEventListener('click', event => {
+    if(event.target.classList.contains('potfolio__nav__list')){
+        PORTFOLIO_TAGS.querySelectorAll('.potfolio__nav__list').forEach(el => el.classList.remove('tag-selected'));
+        event.target.classList.add('tag-selected');
+      
+      for(i = 0; i < PORTFOLIO.children.length; i++){
+        PORTFOLIO.children[i].style.order="0";
+      }
+      if(event.target.id == 'web'){
+        for(let i = 0; i < 12; i+=4){
+          PORTFOLIO.children[i].style.order="1";
+        }
+      }
+      if(event.target.id == 'all') PORTFOLIO.style.flexDirection = 'row';
+      if(event.target.id == 'graphic'){
+        for(let i = 1; i < 12; i+=2){
+          PORTFOLIO.children[i].style.order="2";
+        }
+      }
+      if(event.target.id == 'art'){
+        for(i = 0; i < 12; i+=2){
+          PORTFOLIO.children[i].style.order='3';
+        }
+      }
+    }
+  })
+
+  PORTFOLIO.addEventListener('click', event => {
+    let target = event.target;
+    if (target.tagName == 'IMG') {
+        PORTFOLIO.querySelectorAll('img').forEach(item => {
+            item.style.boxShadow = "none";
+        });
+        event.target.style.boxShadow = "0px 0px 0px 5px #F06C64";
+    }
+  })
